1c2fc2889731f3e8ca21f0c02254dcb078d46357
lazarus2019 <62226062+lazarus2019@users.noreply.github.com>
2021-11-06 06:37:36 +0700

    Upload begin project
    Upload begin project

---
+++ begin project/app.js
@@ -0,0 +1,30 @@
+// ============ VARIABLES ============
+
+
+// ============ CHANGE IMAGE ============
+
+    // Set new image to the top of imageList
+
+
+
+// ============ REMOVE OLD IMAGE ============
+
+
+// ============ SEND IMAGE TO THE BACK ============
+
+
+    // Run looping when the window start
+
+    // Change image when clicked to the bullet
+
+
+                // This bullet is the next index
+
+
+
+    // ============ LOOPING IMAGE ============
+
+
+    // Change current index to the next image
+
+    // ============ RESET FUNCTION ============
